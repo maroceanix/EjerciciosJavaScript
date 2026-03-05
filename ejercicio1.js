9e4d879bd872f167ad485c6e01f3ebac20aa1900
maroceanix <marmoralespalop@gmail.com>
2026-03-05 11:24:00 +0100

    Terminado el ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,28 @@
+/*Ejercicio 1, Nomenclatura y tipo de variables
+Escribe una varible llamada user name utilizando las siguientes nomenclaturas:
+Camel case:
+Pascal case:
+Snake case:
+Kebab case:
+Define los siguientes tipos de variables, di cuál utilizarías y por qué.
+Var:
+Let:
+Const:*/
+
+//Camel case
+ var userName="Mar";
+
+ //Pascal case 
+
+ var UserName="Mar";
+
+ //Kebab casae
+//no se podría poner en kebab case para crear una variable
+
+//Snake case
+var user_name="Mar";
+
+//let: este tipo de variable solo se puede usar dentro de bloque de código
+//var: este tipo de variable es global y se puede definir en cualquier parte del código, no es necesario que sea en un bloque
+//const: este tipo es una constante, la cual su valor no se puede editar, se suele poner: NOMBRE_DE_LA_CONSTANTE para identificarlas a primera vista 
+//He utilizado var porque como no estoy haciendo ningún método ni bloque de código ni nada, es un poco indiferente 
